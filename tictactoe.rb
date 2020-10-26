0a8bf822c8c4fc07d7fc1db21a163136dd254c05
kiizerd <ajsewell13@gmail.com>
2020-10-26 18:01:34 -0500

    added player classes and computer player functionality

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,18 +1,52 @@
 #!/usr/local/bin/ruby -w
 
-class Game
-  def initialize
-    @guide = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-    start_game()
+class Player
+
+  attr_reader :shape, :name
+
+  def initialize(number)
+    @name = "Player #{number}"
+    @score = 0
+    @shape = number == 1 ? "X" : "O"
   end
 
+  def make_move
+    choice = gets.chomp.to_i
+    while choice < 1 || choice > 9
+      puts "Error. Try again. \n"
+      choice = gets.chomp.to_i
+    end
+    choice 
+  end
+end
+
+class Human < Player
+end
+
+class Computer < Player
+  def make_move
+    rand(1..9) 
+  end
+end
+
+class Game
+
+  @@total_games = 0
+  @@x_wins = 0
+  @@o_wins = 0
+
+  def initialize
+    start_game()
+  end
+  
   def start_game
-    @guide = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    @array = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    get_players()
+
+    @array = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
     update_display()
-    @first = ["X", "O"].sample
-    puts "New game started. #{@first} goes first"
+    
+    @first = @players.sample
+    puts "\tNew game started. \n\t#{@first.name} goes first \n" 
     @current = @first
     play_game()
   end
@@ -20,60 +54,86 @@ class Game
   def update_display
     @display = ''
     (1..9).each do |i|
-      @display += i % 3 == 0 ? "#{@array[i]}" + "\n": "#{@array[i]}" + ' | '
+      if i == 1 || i == 4 || i == 7
+        @display += "\t"
+      end
+      @display += i % 3 == 0 ? "#{@array[i]}\n": "#{@array[i]}" + " | "
       if i == 3 || i == 6 
-        @display += "--+---+--\n"
+        @display += "\t--+---+--\n"
       end
     end
     print @display
   end
 
+  def get_players
+    puts "\tHow many human players? (0, 1, 2) \n"
+    @players = gets.chomp.to_i
+    
+    while @players > 2 || @players < 0
+      puts "Error. Try again \n"
+      @players = gets.chomp.to_i
+    end
+
+    case @players
+    when 0
+      player1 = Computer.new(1)
+      player2 = Computer.new(2)
+    when 1
+      player1 = Player.new(1) 
+      player2 = Computer.new(2)
+    when 2
+      player1 = Player.new(1)
+      player2 = Player.new(2)
+    end
+    @players = [player1, player2]
+  end
+
+  def get_move
+    puts "\t#{@current.name}'s turn. \n\tChoose a position. (1-9) \n"
+    choice = @current.make_move
+    while @array[choice] != " "
+      print @display, "\n"
+      puts "Space already taken, try again. \n"
+      choice = @current.make_move
+    end
+    @array[choice] = @current.shape
+  end
+  
   def play_game
     c = 1
     while c <= 9
-      puts "#{@current}'s turn. Choose a position. (1-9)"
-      choice = gets.chomp.to_i
-      while choice < 1 || choice > 9
-        puts "Error. Try again."
-        choice = gets.chomp.to_i
-      end
-      while @array[choice] != " "
-        puts "Space already taken, try again."
-        choice = gets.chomp.to_i
-      end
-      @array[choice] = @current
+      get_move()
       update_display()
       if win_game?
         break
       end
-        @current = @current == "X" ? "O": "X"
+      @current = @current == @players[0] ? @players[1] : @players[0]
       c += 1
     end
+    game_end("none")
   end
 
   def win_game?
-    if
-      @array[5] == "X" && @array[1] == "X" && @array[9] == "X" ||
-      @array[1] == "X" && @array[2] == "X" && @array[3] == "X" ||
-      @array[4] == "X" && @array[5] == "X" && @array[6] == "X" ||
-      @array[7] == "X" && @array[8] == "X" && @array[9] == "X" ||
-      @array[2] == "X" && @array[5] == "X" && @array[8] == "X" ||
-      @array[5] == "X" && @array[3] == "X" && @array[7] == "X" ||
-      @array[1] == "X" && @array[4] == "X" && @array[7] == "X" ||
-      @array[3] == "X" && @array[6] == "X" && @array[9] == "X"
-        game_end("X")
-        return true
-    elsif
-      @array[5] == "O" && @array[1] == "O" && @array[9] == "O" ||
-      @array[1] == "O" && @array[2] == "O" && @array[3] == "O" ||
-      @array[4] == "O" && @array[5] == "O" && @array[6] == "O" ||
-      @array[7] == "O" && @array[8] == "O" && @array[9] == "O" ||
-      @array[2] == "O" && @array[5] == "O" && @array[8] == "O" ||
-      @array[5] == "O" && @array[3] == "O" && @array[7] == "O" ||
-      @array[1] == "O" && @array[4] == "O" && @array[7] == "O" ||
-      @array[3] == "O" && @array[6] == "O" && @array[9] == "O"
-        game_end("O")
-        return true
+    if  @array[5] == "X" && @array[1] == "X" && @array[9] == "X" ||
+        @array[1] == "X" && @array[2] == "X" && @array[3] == "X" ||
+        @array[4] == "X" && @array[5] == "X" && @array[6] == "X" ||
+        @array[7] == "X" && @array[8] == "X" && @array[9] == "X" ||
+        @array[2] == "X" && @array[5] == "X" && @array[8] == "X" ||
+        @array[5] == "X" && @array[3] == "X" && @array[7] == "X" ||
+        @array[1] == "X" && @array[4] == "X" && @array[7] == "X" ||
+        @array[3] == "X" && @array[6] == "X" && @array[9] == "X"
+          game_end("X")
+          return true
+    elsif @array[5] == "O" && @array[1] == "O" && @array[9] == "O" ||
+          @array[1] == "O" && @array[2] == "O" && @array[3] == "O" ||
+          @array[4] == "O" && @array[5] == "O" && @array[6] == "O" ||
+          @array[7] == "O" && @array[8] == "O" && @array[9] == "O" ||
+          @array[2] == "O" && @array[5] == "O" && @array[8] == "O" ||
+          @array[5] == "O" && @array[3] == "O" && @array[7] == "O" ||
+          @array[1] == "O" && @array[4] == "O" && @array[7] == "O" ||
+          @array[3] == "O" && @array[6] == "O" && @array[9] == "O"
+            game_end("O")
+            return true
     end
     return false
   end
@@ -82,22 +142,22 @@ class Game
     @winner = winner
     case winner
     when "X"
-      puts "X wins!"
+      puts "\tX wins! \n"
+      @@x_wins += 1
     when "O" 
-      puts "O wins!"
+      puts "\tO wins! \n"
+      @@o_wins += 1
     else
-      puts "No winner."
+      puts "\tNo winner. \n"
     end
-    puts "@first: #{@first}"
-    puts "@current: #{@current}"
-    puts "@winner #{@winner}"
-    puts "Play again? (Y/N)"
+    @@total_games += 1
+    puts "\tPlay again? (Y/N) \n"
     @new_game = gets.chomp.upcase
     if @new_game == "Y"
-      puts "Starting new game!"
+      puts "\tStarting new game! \n"
       start_game()
     elsif @new_game == "N"
-      puts "Goodbye"
+      puts "\tGoodbye \n"
       exit
     else
       exit
